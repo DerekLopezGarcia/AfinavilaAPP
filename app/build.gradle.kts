e8cee96dc25af5d11d987530f41af25c2ed7eacd
DerekLopezGarcia <dereklopezgarcia@gmail.com>
2026-08-21 10:34:22 +0200

    release: prepare Android 0.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         applicationId = "es.afinavila"
         minSdk = 21
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.2.0"
+        versionCode = 3
+        versionName = "0.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
