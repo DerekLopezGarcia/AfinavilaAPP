97185ba7f9f8701658ee90a6b5f37c4ff0b349eb
DerekLopezGarcia <dereklopezgarcia@gmail.com>
2026-08-21 14:39:12 +0200

    fix: add server certificate trust for SSL on older devices

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         applicationId = "es.afinavila"
         minSdk = 21
         targetSdk = 35
-        versionCode = 3
-        versionName = "0.3.0"
+        versionCode = 7
+        versionName = "0.3.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
@@ -121,4 +121,5 @@ dependencies {
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
 
 }
+
 
